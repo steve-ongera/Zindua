66337f5907f9e3513769c5d30916fccc698fe6da
steve-ongera <gadafisteve001@gmail.com>
2025-03-02 14:14:51 +0300

    made new changes at 2:14 pm 3/2/2025

--- myapp/models.py
+++ myapp/models.py
@@ -165,6 +165,7 @@ class PickupStation(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=200)
     details = models.TextField()
+    delivery_fee = models.DecimalField(max_digits=10, decimal_places=2 , null= True , blank=True)
     
     def __str__(self):
         return self.name
